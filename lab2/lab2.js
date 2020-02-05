32b9aa06addd6b50d700b5942c129b6ecfcb6fd2
Aidan Rooney <aidanr1999@hotmail.com>
2020-02-05 10:32:56 +0000

    task 3

--- lab2/lab2.js
+++ lab2/lab2.js
@@ -8,31 +8,21 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 
-// index page
-app.get('/', function(req, res){
-    res.type('text/plain');
-    res.send('Landing Page');
+app.get('/', function (req, res) {
+    res.render('index');
 });
-
-// about page
-app.get('/about', function(req, res){
-      res.type('text/plain');
-      res.send('The about page');
+app.get('/about', function (req, res) {
+    res.render('about');
 });
-
-// custom 404 page
-app.use(function (req, res) {
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 - Not Found');
+// 404 catch-all handler (middleware)
+app.use(function (req, res, next) {
+    res.status(404); res.render('404');
 });
-// custom 500 page
+// 500 error handler (middleware)
 app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.type('text/plain');
-    res.status(500);
-    res.send('500 - Server Error');
-});
+    console.error(err.stack); res.status(500); res.render('500');
+})
+
 app.listen(app.get('port'), function () {
     console.log('Express started on http://localhost:' + app.get('port') +
         '; press Ctrl-C to terminate.');
